c8c9aaa8782d60ec9227f1edc4b227870f1a895d
Fabian Steiner <fabianste@edu.aau.at>
2023-05-01 16:47:12 +0200

    Implementation of basic functionality of PlayerManager and Player, adjustment of TurnManager

--- app/src/main/java/com/example/se2_exploding_kittens/Network/PlayerManager.java
+++ app/src/main/java/com/example/se2_exploding_kittens/Network/PlayerManager.java
@@ -1,9 +1,59 @@
 package com.example.se2_exploding_kittens.Network;
 
 import com.example.se2_exploding_kittens.Network.TCP.ServerTCPSocket;
+import com.example.se2_exploding_kittens.NetworkManager;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class PlayerManager {
+public class PlayerManager implements MessageCallback {
+    private static final int PLAYER_MANAGER_MESSAGE_ID = 400;
+    private List<Player> players;
+    private int nextPlayerID;
 
+    public PlayerManager(NetworkManager networkManager) {
+        networkManager.subscribeCallbackToMessageID(this, PLAYER_MANAGER_MESSAGE_ID);
+        this.players = new ArrayList<>();
+        this.nextPlayerID = 0;
+    }
+
+    public void assignPlayerID(ServerTCPSocket connection) {
+        int playerID = nextPlayerID++;
+        Player player = new Player(connection, playerID);
+        players.add(player);
+    }
+
+    public Player getPlayer(int playerId) {
+        for (Player player : players) {
+            if (player.getPlayerID() == playerId) {
+                return player;
+            }
+        }
+        return null; // player not found
+    }
+
+    public int getPlayerSize() {
+        return players.size();
+    }
+
+    public void shuffle() {
+        Collections.shuffle(players);
+    }
+
+    public int getPlayerIDByConnection(ServerTCPSocket connection) {
+        for (Player player : players) {
+            if (player.getConnection() == connection) {
+                return player.getPlayerID();
+            }
+        }
+        return -1; // player not found
+    }
+
+    @Override
+    public void responseReceived(String text, Object sender) {
+        if (sender instanceof ServerTCPSocket) {
+            assignPlayerID((ServerTCPSocket) sender);
+        }
+    }
 }
--- app/src/main/java/com/example/se2_exploding_kittens/TurnManager.java
+++ app/src/main/java/com/example/se2_exploding_kittens/TurnManager.java
@@ -3,38 +3,41 @@ package com.example.se2_exploding_kittens;
 import com.example.se2_exploding_kittens.Network.Message;
 import com.example.se2_exploding_kittens.Network.MessageCallback;
 import com.example.se2_exploding_kittens.Network.MessageType;
+import com.example.se2_exploding_kittens.Network.Player;
+import com.example.se2_exploding_kittens.Network.PlayerManager;
 import com.example.se2_exploding_kittens.Network.TCP.ServerTCPSocket;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class TurnManager implements MessageCallback {
     public static final int TURN_MANAGER_MESSAGE_ID = 300;
-    private ArrayList<ServerTCPSocket> players;
     private NetworkManager networkManager;
-    private int playerIndex;
+    private PlayerManager playerManager;
+    private int currentPlayerIndex;
 
     public TurnManager(ArrayList<ServerTCPSocket> players, NetworkManager networkManager) {
-        networkManager.subscribeCallbackToMessageID(this,TURN_MANAGER_MESSAGE_ID);
-        this.players = players;
-        //this.players
+        networkManager.subscribeCallbackToMessageID(this, TURN_MANAGER_MESSAGE_ID);
+        this.playerManager = new PlayerManager(networkManager);
+        for (ServerTCPSocket player : players) {
+            playerManager.assignPlayerID(player);
+        }
         this.networkManager = networkManager;
-        this.playerIndex = 0;
+        this.currentPlayerIndex = 0;
     }
 
     public void startGame() {
         shuffleOrder();
+        currentPlayerIndex = 0;
         sendGameSateToClients();
     }
 
     private void shuffleOrder() {
-        Collections.shuffle(players);
-        playerIndex = 0;
+        playerManager.shuffle();
     }
-    //1:1:2       ID1=players <n> Turn
-    //2:2:2       ID1=players <n> Turn
+
     private void sendGameSateToClients() {
-        String gameStateMessage = "It's " + players.get(playerIndex) + "s Turn.";
+        Player currentPlayer = playerManager.getPlayer(currentPlayerIndex);
+        String gameStateMessage = Integer.toString(currentPlayer.getPlayerID());
 
         try {
             networkManager.sendMessageBroadcast(new Message(MessageType.MESSAGE, TURN_MANAGER_MESSAGE_ID, gameStateMessage));
@@ -44,30 +47,23 @@ public class TurnManager implements MessageCallback {
     }
 
     private void updateGameState() {
-        playerIndex = (playerIndex + players.size() - 1) % players.size();
+        currentPlayerIndex = (currentPlayerIndex + 1) % playerManager.getPlayerSize();
         sendGameSateToClients();
     }
 
-    private ServerTCPSocket getCurrentPlayer() {
-        return players.get(playerIndex);
-    }
-
-    public void handlePlayerAction(ServerTCPSocket player) {
-        if (player != getCurrentPlayer()) {
-            sendErrorMessageToPlayer(player, "It's not your turn.");
+    public void handlePlayerAction(int playerID) {
+        if (currentPlayerIndex != playerID) {
+            sendErrorMessageToPlayer(playerID, "It's not your turn.");
             return;
         }
-
-        // Handle player action here
 
         updateGameState();
         sendGameSateToClients();
     }
 
-    private void sendErrorMessageToPlayer(ServerTCPSocket player, String errorMessage) {
+    private void sendErrorMessageToPlayer(int playerID, String errorMessage) {
         try {
-            //networkManager.sendMessageFromTheSever(new Message(MessageType.ERROR, TURN_MANAGER_MESSAGE_ID, errorMessage), plavermanger.getConnection(id));
-            networkManager.sendMessageFromTheSever(new Message(MessageType.ERROR, TURN_MANAGER_MESSAGE_ID, errorMessage), player);
+            networkManager.sendMessageFromTheSever(new Message(MessageType.ERROR, TURN_MANAGER_MESSAGE_ID, errorMessage), playerManager.getPlayer(playerID).getConnection());
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
@@ -75,9 +71,8 @@ public class TurnManager implements MessageCallback {
 
     @Override
     public void responseReceived(String text, Object sender) {
-        //this runs on server
-        if(sender instanceof ServerTCPSocket){
-            //int player = playermanager.getPlayerID((serverTPCSocket)sender)
+        if (sender instanceof ServerTCPSocket) {
+            int player = playerManager.getPlayerIDByConnection((ServerTCPSocket) sender);
             String message = Message.parseAndExtractPayload(text);
         }
     }
