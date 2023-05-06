1483e88c81408a4551acb8512a414d778901c67d
amadeovalar <56834491+amadeovalar@users.noreply.github.com>
2023-05-06 16:55:40 +0200

    Drag and drop feature is implemented for 1 player.

--- app/src/main/java/com/example/se2_exploding_kittens/MainActivity.java
+++ app/src/main/java/com/example/se2_exploding_kittens/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.se2_exploding_kittens;
 
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.util.Log;
-
 import com.example.se2_exploding_kittens.cards.AttackCard;
 import com.example.se2_exploding_kittens.cards.Cards;
-import com.example.se2_exploding_kittens.cards.Deck.Deck;
+import com.example.se2_exploding_kittens.cards.DefuseCard;
+import com.example.se2_exploding_kittens.cards.FavorCard;
+import com.example.se2_exploding_kittens.cards.NopeCard;
+import com.example.se2_exploding_kittens.cards.SkipCard;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -27,6 +34,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Implement onDragListener for the discard pile view
+        View discardPileView = findViewById(R.id.discardPile);
+        discardPileView.setOnDragListener(new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case DragEvent.ACTION_DROP:
+                        // Get the dragged item from the ClipData object
+                        ClipData.Item item =  event.getClipData().getItemAt(0);
+                        String cardResourceString = String.valueOf(item.getText());
+                        int cardResource =  Integer.parseInt(cardResourceString);
+                        // Add the card to the discard pile
+                        ImageView discardedCard = new ImageView(MainActivity.this);
+                        discardedCard.setImageResource(cardResource);
+
+                        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                                ViewGroup.LayoutParams.WRAP_CONTENT,
+                                ViewGroup.LayoutParams.WRAP_CONTENT
+                        );
+                        discardedCard.setLayoutParams(params);
+                        ((ViewGroup) v).addView(discardedCard);
+                        // Setting image at the beginning to the invisible state
+                        ImageView discardImage = findViewById(R.id.discard_pile_image);
+                        discardImage.setVisibility(View.INVISIBLE);
+
+                }
+                return true;
+            }
+        });
 
         // Initialize the RecyclerView and layout manager
         recyclerView = findViewById(R.id.recyclerVw);
@@ -41,6 +78,10 @@ public class MainActivity extends AppCompatActivity {
         // Initialize the list of cards and the adapter
         cardList = new ArrayList<Cards>();
         cardList.add(new AttackCard());
+        cardList.add(new DefuseCard());
+        cardList.add(new NopeCard());
+        cardList.add(new FavorCard());
+        cardList.add(new SkipCard());
 
         // Add more cards as needed
 
@@ -49,6 +90,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the adapter for the RecyclerView
         recyclerView.setAdapter(adapter);
+
+
+
 
 
     }
